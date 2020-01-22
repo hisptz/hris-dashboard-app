f786e1837868fcd828ab941846d4d7695c8db126
Rajab Mkomwa <rajeyw@gmail.com>
2020-01-22 15:47:18 +0300

    fix: resolve menu search styling issues

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,31 @@ import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { pipes } from './pipes';
 
 @NgModule({
-  imports: [CommonModule, MatSelectModule, MatChipsModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   declarations: [...pipes],
-  exports: [...pipes, MatSelectModule, MatChipsModule, MatButtonModule]
+  exports: [
+    ...pipes,
+    MatSelectModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ]
 })
 export class SharedModule {}
